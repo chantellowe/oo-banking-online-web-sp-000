e4e3655a3c7b046293d001547d8646e7c492139c
Chantel Lowe <chantellowe17@gmail.com>
2019-09-21 18:27:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
   
   def execute_transaction()
-    if @sender.valid? && @status == "pending"
+    if @sender.valid? #&& @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
